77f4ec0f62855f9b52bbbdd3f18c771f635f3565
olehedza <BiewZpo2>
2020-08-04 23:40:34 +0300

    first commit

---
+++ src/main/java/com/olehedza/quicknews/QuickNewsApplication.java
@@ -0,0 +1,11 @@
+package com.olehedza.quicknews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickNewsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(QuickNewsApplication.class, args);
+	}
+}
